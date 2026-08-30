4cbc5a876588eb14f9aca2b3535d7f8d2a406180
Karollo6767 <karol.stanek12@gmail.com>
2026-08-30 18:11:32 +0200

    Change code logic: replace setting time by hand in code with automatic time & date downloading from PC when compiling and pushing the code to Arduino.

--- RTC_RGB_clock.ino
+++ RTC_RGB_clock.ino
@@ -4,6 +4,9 @@
 #define greenPin 10
 #define bluePin 11
 
+//variable to ensure, that time in serial port change only once per minute 
+int lastMin = -1;
+
 void setup() {
   
   Serial.begin(9600);
@@ -11,16 +14,41 @@ void setup() {
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
 
-  //set start date and hour (format: hour, minute, second, day, month, year)
-  setTime(16, 15, 0, 30, 8, 2026);
+  //AUTO TIME SYNC:
+  //extract the hour, minute, and second from the computer's compilation time (__TIME__)
+  int hr = cstr2int(__TIME__);
+  int min = cstr2int(__TIME__ + 3);
+  int sec = cstr2int(__TIME__ + 6);
+
+  //extract day, month, year from computer's date (__DATE__)
+  int d = cstr2int(__DATE__ + 4);
+  int yr = cstr2int(__DATE__ + 7);
+  int mo = 1;
+
+  //automatic align of the month from PC (e.g. "Aug") to (e.g. 8)
+  const char *months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+  for (int i = 0; i < 12; i++) {
+    if (strncmp(__DATE__, months[i], 3) == 0) {
+      mo = i + 1;
+      break;
+    }
+  }
+
+  //set time fetched form computer's time & date obtianed in time of compilation
+  setTime(hr, min, sec, d, mo, yr);
 
 }
 
 void loop() {
 
   setColor(255, 0, 0);
-  displayTime();
-  delay(1000);
+
+  //I could do it with delay(60000) (60000ms = 1min), but delay stops the program for minute
+  //so I check if minute has changed and then instantly Arduino prints time with next minute
+  if (minute() != lastMin) {
+    lastMin = minute(); //save current minute
+    displayTime();             
+  }
 
 }
 
@@ -33,15 +61,13 @@ void setColor(int red, int green, int blue) {
 }
 
 void displayTime() {
-  //display time 
+  //time displaying
   addZeroIfNumNotTen(hour());
   Serial.print(":");
   addZeroIfNumNotTen(minute());
-  Serial.print(":");
-  addZeroIfNumNotTen(second());
-  Serial.print(" ");
+  Serial.print(" | ");
 
-  //display date
+  //date displaying
   addZeroIfNumNotTen(day());
   Serial.print(".");
   addZeroIfNumNotTen(month());
@@ -51,10 +77,21 @@ void displayTime() {
 
 }
 
-//function which adds 0 if number is below 10
+//func that adds 0 before numbers if those are lower than 10
 void addZeroIfNumNotTen(int num) {
-  if (num < 10) {
-    Serial.print('0');
-  }
+
+  if (num < 10) Serial.print('0');
   Serial.print(num);
+
+}
+
+// NOWA FUNKCJA POMOCNICZA:
+// Przetwarza wewnętrzny tekst kompilatora (np. "17") na prawdziwą liczbę int (17)
+int cstr2int(const char *str) {
+  int numer = 0;
+  while (*str >= '0' && *str <= '9') {
+    numer = numer * 10 + (*str - '0');
+    str++;
+  }
+  return numer;
 }
